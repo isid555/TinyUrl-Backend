b741aeabdb76e45390af60c045f9b54d3e6b3a57
isid555 <r555sid@gmail.com>
2026-05-15 00:08:02 +0530

    Cors Issue

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const urlRoutes = require('./routes/urlRoutes');
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'https://tiny-url-ochre.vercel.app'
+    ],
+    credentials: true
+}));
 app.use(express.json());
 
 // mongoose.connect(process.env.MONGO_URI, () => console.log('MongoDB Connected Successffully'));
